8e33b6e0fa3aea5037f2d3e39a729a9093831772
romainb22 <58576605+romainb22@users.noreply.github.com>
2021-02-24 16:47:56 +0100

    Updated

--- triangle.c
+++ triangle.c
@@ -1,6 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+void draw_line(int i,int j){
+  int x,y;
+  for (x=0;x<atoi(j)+1-i;x++){
+    printf(" ");
+  }
+  for(y=0;y<=i*2;y++){
+    printf("*");
+  }
+  printf("\n");
+}
+
+
 int main(int argc, char* argv[]){
 
   int a,b,c=0,d;
@@ -21,18 +33,7 @@ int main(int argc, char* argv[]){
   }
   printf("\n");
   for(a=0;a<atoi(argv[1]);a++){
-    draw_line(a);
+    draw_line(a, atoi(argv[1]));
   }
   return 0;
-}
-
-void draw_line(int i){
-  int x,y;
-  for (x=0;x<atoi(argv[1])+1-i;x++){
-    printf(" ");
-  }
-  for(y=0;y<=i*2;y++){
-    printf("*");
-  }
-  printf("\n");
 }
