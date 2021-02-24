0d82e430b0b25f22c78dd452df05ce60dcd3b141
romainb22 <58576605+romainb22@users.noreply.github.com>
2021-02-24 16:36:35 +0100

    Updated

--- triangle.c
+++ triangle.c
@@ -4,16 +4,10 @@
 int main(int argc, char* argv[]){
 
   int a,b,c=0,d;
-
-  if(argc > 2){
-    printf("Trop d'arguments !\n");
-    return -1;
-  }
-
   /* version itérative */
 
   for(a=0;a<atoi(argv[1]);a++){
-    for(b=0;b<atoi(argv[1])/2+1-c;b++){
+    for(b=0;b<atoi(argv[1])+1-c;b++){
       printf(" ");
     }
     for(d=0;d<=c*2;d++){
